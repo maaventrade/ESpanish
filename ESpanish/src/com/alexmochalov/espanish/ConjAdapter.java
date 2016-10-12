5c368a10ec98d6bc8f2bd97787b50709d06fe3db
maaventrade <maaventrade@gmail.com>
2016-10-12 12:02:14 +0400

    12

--- ESpanish/src/com/alexmochalov/espanish/ConjAdapter.java
+++ ESpanish/src/com/alexmochalov/espanish/ConjAdapter.java
@@ -16,121 +16,162 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.TextView;
+import com.alexmochalov.espanish.ConjAdapter.*;
 
-public class ConjAdapter extends BaseAdapter {
+public class ConjAdapter extends BaseAdapter
+{
 	Context mContext;
 	LayoutInflater layoutInflater;
-	
-	static class PronounChecked {
-		public PronounChecked(Pronoun pronoun) {
+
+	public boolean allChecked()
+	{
+		for (PronounChecked p: objects)
+			if (!p.checked) 
+				return false;
+		return true;
+	}
+
+	public void test(String translation, int i)
+	{
+		objects.get(i).checked = 
+			translation.
+			replaceAll("á", "a").
+			replaceAll("ó", "o").
+			replaceAll("ú", "u").
+			replaceAll("á", "a").
+			toLowerCase().
+			equals(objects.get(i).mPronoun.conj(mVerb).
+				   replaceAll("á", "a").
+				   replaceAll("ó", "o").
+				   replaceAll("ú", "u").
+				   replaceAll("á", "a").
+				   toLowerCase());
+
+	}
+
+	static class PronounChecked
+	{
+		public PronounChecked(Pronoun pronoun)
+		{
 			mPronoun = pronoun;
 			checked = false;
 		}
 		Pronoun mPronoun;
 		Boolean checked;
+
+		public void put(ConjAdapter.PronounChecked from)
+		{
+			mPronoun = from.mPronoun;
+		}
+
+		public ConjAdapter.PronounChecked getCopy()
+		{
+			return new PronounChecked(mPronoun);
+		}
 	};
-	
+
 	ArrayList<PronounChecked> objects = new ArrayList<PronounChecked>();
-	
+
 	String mVerb = "";
-		
+
 	private boolean mAnswer = false;
-	  
-	public ConjAdapter(Context context, String verb) {
+
+	public ConjAdapter(Context context, String verb)
+	{
 	    mContext = MainActivity.mContext;
 	    mVerb = verb;
-	    
-	    for (Pronoun p: Dictionary.getPronouns()){
+
+	    for (Pronoun p: Dictionary.getPronouns())
+		{
 	    	objects.add(new PronounChecked(p));
 		}
-	    
+
+		for (int i = 1; i <= objects.size(); i++){
+			int j = (int)Math.random() * objects.size();
+			int k = (int)Math.random() * objects.size();
+			
+			PronounChecked p = objects.get(i).getCopy();
+			objects.get(i).put(objects.get(j));
+			objects.get(j).put(p);
+			
+		}
+		
+		
 	    layoutInflater = (LayoutInflater) mContext
 	        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	  }
-	
-	public void setAnswer(boolean answer){
+	}
+
+	public void setAnswer(boolean answer)
+	{
 		mAnswer = answer;
-		if (answer){
-			
+		if (answer)
+		{
+
 		}
 		else 
 			for (PronounChecked p: objects)
 				p.checked = false;
 	}
-	
-	public void setVerb(String verb){
+
+	public void setVerb(String verb)
+	{
 		mVerb = verb;
 	}    
-	
+
 	@Override
-	public int getCount() {
+	public int getCount()
+	{
 		return objects.size();
 	}
 
 	@Override
-	public Object getItem(int position) {
+	public Object getItem(int position)
+	{
 		return objects.get(position);
 	}
 
 	@Override
-	public long getItemId(int position) {
+	public long getItemId(int position)
+	{
 		return position;
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
+	public View getView(int position, View convertView, ViewGroup parent)
+	{
 		View view = convertView;
-	    if (view == null) {
-	      view = layoutInflater.inflate(R.layout.fragment_conj_item, parent, false);
+	    if (view == null)
+		{
+			view = layoutInflater.inflate(R.layout.fragment_conj_item, parent, false);
 	    }
-	 
+
 	    PronounChecked pronoun = (PronounChecked)getItem(position);
-	 
+
 	    ((TextView) view.findViewById(R.id.text)).setText(pronoun.mPronoun.mText);
 	    ((TextView) view.findViewById(R.id.translation)).setText(pronoun.mPronoun.translation);
-	    
+
 		EditText EditTextTranslation;
 	    EditTextTranslation = ((EditText) view.findViewById(R.id.EditTextTranslation));
-	    
-	    EditTextTranslation.addTextChangedListener(new TextWatcher() {
-
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count,
-					int after) {
-			}
-
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before,
-					int count) {
-			}
-
-			@Override
-			public void afterTextChanged(Editable s) {
-				Log.d("", "S "+s);
-			}
-	    	
-	    });
 
 
-	    if (mAnswer){
-	    	if (pronoun.checked);
-	    	else {
-	        	String translation = EditTextTranslation.getText().toString();
-	        	Log.d("", "position "+position+" EditTextTranslation "+translation);
-	        	
-	        	if (translation.toLowerCase().equals(pronoun.mPronoun.conj(mVerb).toLowerCase() ))
-	        		EditTextTranslation.setTextColor(Color.GREEN);
-	        	else 
-	        		EditTextTranslation.setTextColor(Color.RED);
-	        	pronoun.checked = true;
-	    	}
-	    	EditTextTranslation.setText( pronoun.mPronoun.conj(mVerb));
-	    } else {
-	    	EditTextTranslation.setTextColor(Color.BLACK);
-	    	EditTextTranslation.setText( "" );
+
+
+	    if (mAnswer)
+		{
+	    	if (pronoun.checked)
+				EditTextTranslation.setTextColor(Color.GREEN);
+	        else 
+	        	EditTextTranslation.setTextColor(Color.RED);
+
+	    	EditTextTranslation.setText(pronoun.mPronoun.conj(mVerb));
 	    }
-	    
+		else
+		{
+	    	EditTextTranslation.setTextColor(Color.BLACK);
+	    	EditTextTranslation.setText("");
+	    }
+
 	    return view;	
-	 }
+	}
 
 }
--- ESpanish/src/com/alexmochalov/espanish/FragmentConj.java
+++ ESpanish/src/com/alexmochalov/espanish/FragmentConj.java
@@ -23,129 +23,134 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class FragmentConj extends Fragment {
+public class FragmentConj extends Fragment
+{
 	private SharedPreferences prefs;
 	private Fragment thisFragment;
 
 	private String text;
 	// Current translation
 	private String translation;
-	
+
 	private int index = 0;
-	
+
 	private TextView mText;
 	private TextView mTranslation;
-	
+
 	private ConjAdapter conjAdapter;
 	private ListView listView;
-	
+
 	private View rootView;
 	private Button button_test;
 	//private Context mContext;
-	
+
 	/*
-	public static final FragmentConj newInstance(int title, String message)
+	 public static final FragmentConj newInstance(int title, String message)
+	 {
+	 FragmentConj f = new FragmentConj();
+	 Bundle bdl = new Bundle(2);
+	 //bdl.putInt(EXTRA_TITLE, title);
+	 //bdl.putString(EXTRA_MESSAGE, message);
+	 //f.setArguments(bdl);
+	 return f;
+	 }	
+	 */
+
+	/*	
+	 public FragmentConj(Context context) {
+	 mContext = context;
+	 thisFragment = this;
+	 }
+	 */
+
+    private void next()
 	{
-		FragmentConj f = new FragmentConj();
-	    Bundle bdl = new Bundle(2);
-	    //bdl.putInt(EXTRA_TITLE, title);
-	    //bdl.putString(EXTRA_MESSAGE, message);
-	    //f.setArguments(bdl);
-	    return f;
-	}	
-	*/
-	
-/*	
-    public FragmentConj(Context context) {
-    	mContext = context;
-    	thisFragment = this;
-    }
- */
- 
-    private void next() {
     	index = DrawerMenu.next();
-    	
+
         mText = (TextView)rootView.findViewById(R.id.text);
         mTranslation = (TextView)rootView.findViewById(R.id.translation);
-        
+
 		text = DrawerMenu.getText(index);
-		
+
 		mText.setText(text);
 		mTranslation.setText(Dictionary.getTranslation(text));
-		
+
 		conjAdapter.setVerb(text);
 		//conjAdapter.notifyDataSetChanged();
 	}
-    
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
- 
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+
         rootView = inflater.inflate(R.layout.fragment_conj, container, false);
-        
+
 		conjAdapter = new ConjAdapter(MainActivity.mContext, text);
 	    listView = (ListView)rootView.findViewById(R.id.conjugations);
 	    listView.setAdapter(conjAdapter);			
 
     	next();
-	    
+
 	    button_test = (Button)rootView.findViewById(R.id.button_conj_test);
 	    button_test.setOnClickListener(new OnClickListener(){
 
-			@Override
-			public void onClick(View v) {
+				@Override
+				public void onClick(View v)
+				{
 
-				// Button Next is pressed
-				if (button_test.getText().equals(MainActivity.mContext.getResources().getString(R.string.button_next))){
-					next();
-					button_test.setText(MainActivity.mContext.getResources().getString(R.string.button_test));
-					conjAdapter.setAnswer(false);
-					conjAdapter.notifyDataSetChanged();
-				}	
-				else {
-					// Button Проверить is pressed
-					
-					button_test.setText(MainActivity.mContext.getResources().getString(R.string.button_next));
-					
-					EditText EditTextTranslation;
-					
-					for (int i = 0; i < Dictionary.getPronouns().size()-1; i++){
-						View view = conjAdapter.getView(i, null, listView);
-					    EditTextTranslation = ((EditText) view.findViewById(R.id.EditTextTranslation));
-						
-			        	String translation = EditTextTranslation.getText().toString();
-			        	Log.d("", "pos "+i+" EditTextTranslation "+translation);
-			        	
-			        	//if (translation.toLowerCase().equals(pronoun.mPronoun.conj(mVerb).toLowerCase() ))
-					}
-					
-					
-					conjAdapter.setAnswer(true);
-					conjAdapter.notifyDataSetChanged();
-//					EditText editText = (EditText)rootView.findViewById(R.id.editText_phrase_transl);
-/*
-					TextView mTranslation = (TextView)rootView.findViewById(R.id.TextViewPhraseTranslation);
-					
-					boolean result = Dictionary.testRus(editText.getText().toString(), mTranslation.getText().toString(), direction);
-					
-					// result = true;
-					mTranslation.setVisibility(View.VISIBLE);
-					if (result){
-						mTranslation.setTextColor(getColor(mContext, R.color.green1));
-						DrawerMenu.setStepCompleted(index, typeOfstep);
-						if (DrawerMenu.getDataSize() == 0){
-							Toast.makeText(mContext, "THAT IS ALL", Toast.LENGTH_LONG).show();
-							// Close Fragment
-							getActivity().getFragmentManager().beginTransaction().remove(thisFragment).commit();
+					// Button Next is pressed
+					if (button_test.getText().equals(MainActivity.mContext.getResources().getString(R.string.button_next)))
+					{
+						next();
+						button_test.setText(MainActivity.mContext.getResources().getString(R.string.button_test));
+						conjAdapter.setAnswer(false);
+						conjAdapter.notifyDataSetChanged();
+					}	
+					else
+					{
+						// Button Проверить is pressed
+
+						button_test.setText(MainActivity.mContext.getResources().getString(R.string.button_next));
+
+						EditText EditTextTranslation;
+
+						for (int i = 0; i <= Dictionary.getPronouns().size() - 1; i++)
+						{
+							View view = listView.getChildAt(i);
+							EditTextTranslation = ((EditText) view.findViewById(R.id.EditTextTranslation));
+
+							String translation = EditTextTranslation.getText().toString();
+							
+
+							conjAdapter.test(translation, i);
+
 						}
-					}
-					else 
-						mTranslation.setTextColor(Color.RED);
-					*/
+
+
+						conjAdapter.setAnswer(true);
+						conjAdapter.notifyDataSetChanged();
+
+						if (conjAdapter.allChecked())
+						{
+							DrawerMenu.setStepCompleted(index, 3);
+							TextView textView = ((TextView) rootView.findViewById(R.id.TextViewConjInfo));
+							textView.setText(DrawerMenu.getCountStr());
+							
+							if (DrawerMenu.getDataSize() == 0)
+							{
+								Toast.makeText(MainActivity.mContext, "THAT IS ALL", Toast.LENGTH_LONG).show();
+								// Close Fragment
+								getActivity().getFragmentManager().beginTransaction().remove(thisFragment).commit();
+							}
+						}
+					
+
 				}	
-				
+
 			}});
-		
-        return rootView;
+		TextView textView = ((TextView) rootView.findViewById(R.id.TextViewConjInfo));
+		textView.setText(DrawerMenu.getCountStr());
+	return rootView;
     }
 
 
