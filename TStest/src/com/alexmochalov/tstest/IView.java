22b6f727d137077f3d22147808835f0a5211e818
maaventrade <maaventrade@gmail.com>
2018-05-11 18:03:19 +0300

    1802

--- TStest/src/com/alexmochalov/tstest/IView.java
+++ TStest/src/com/alexmochalov/tstest/IView.java
@@ -1,23 +1,61 @@
 package com.alexmochalov.tstest;
 
+import java.util.ArrayList;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class IView extends ImageView {
+public class IView extends View {
 
 	private TextView mTv;
 	private Canvas canvas;
+	private ArrayList<Item> items = new ArrayList<Item>();
 
+	private Bitmap mBitmap;
+	private Rect mRect = new Rect();
+	private Rect mRectBitmap = new Rect();
+	
+	private Bitmap mBitmapBg;
+	private Rect mRectBG;
+
+	private Brush mBrush;
+	
+	private int width = 1000;
+	private int height = 1000;
+	
+	private float offsetX = 0;  
+	private float offsetY = 0;  
+	private float kZooming = 1; 
+	
+	private Canvas mCanvas;
+	
+	private int idBG;
+
+
+	private byte r[][];
+	private byte y[][];
+	private byte b[][];
+	private byte w[][];
+	private byte t[][];
+	private boolean modified[][];
+
+	
+	private Paint mPaint = new Paint();
+	
 	public IView(Context context, AttributeSet attrs, int defStyleAttr
 			) {
 		super(context, attrs, defStyleAttr);
@@ -34,38 +72,102 @@ public class IView extends ImageView {
 		super(context, attrs, defStyleAttr, defStyleRes);
 	}
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
-        
-        if (this.getHeight() > 0){
-        	
-        	Bitmap bitmap = Bitmap.createBitmap(this.getWidth(), this.getHeight(), Bitmap.Config.ARGB_8888);    
-            canvas = new Canvas(bitmap);
-            
-            setImageBitmap(bitmap);
-        }
-    }
+	@Override
+	protected void onSizeChanged(int wd, int h, int oldw, int oldh) {
+		super.onSizeChanged(wd, h, oldw, oldh);
+
+		width = Math.max(wd, h);
+		height = Math.max(wd, h);
+
+		if (isInEditMode())
+			return;
+
+		load();
+
+		if (offsetX > width)
+			offsetX = 0;
+		if (offsetY > height)
+			offsetY = 0;
+
+		mRect.set(0, 0, (int) (width * kZooming), (int) (height * kZooming));
+		//mRect.offset(offsetX, offsetY);
+
+		//int i = prefs.getInt(PREFS_BRUSH_TRANSP, 100);
+		//int j = prefs.getInt(PREFS_BRUSH_SIZE, 1003);
+
+		//brush.setTransparency(i);
+		//brush.setSize(j, width);
+	}
+
+	public void setBrush(Brush brush)
+	{
+		mBrush = brush;
+	}
 	
-	
-    @Override
+    private void load() {
+
+    	Bitmap newBitmap = Bitmap.createBitmap(width, height,
+				Bitmap.Config.ARGB_8888);
+
+		Canvas newCanvas = new Canvas();
+		newCanvas.setBitmap(newBitmap);
+		if (mBitmap != null) {
+			newCanvas.drawBitmap(mBitmap, 0, 0, null);
+		}
+		
+		mBitmap = newBitmap;
+		mCanvas = newCanvas;
+		
+		mRectBitmap = new Rect(0,0, width, height);
+		
+		r = new byte[width][height];
+		y = new byte[width][height];
+		b = new byte[width][height];
+		w = new byte[width][height];
+		t = new byte[width][height];
+		modified = new boolean[width][height];
+
+		for (int x = 0; x < width; x++)
+			for (int z = 0; z < height; z++){
+				r[x][z] = 0;
+				y[x][z] = 0;
+				b[x][z] = 0;
+				w[x][z] = 0;
+				t[x][z] = 0;
+				modified[x][z] = false;
+			}
+	}
+
+	@Override
     public boolean onTouchEvent(MotionEvent event) {
      
-    	Paint paint = new Paint();
-    	paint.setColor(Color.BLACK);
+		int N = event.getHistorySize();
+		
+		for (int i=0; i<N; i++) {
+			drawPoint((int)((event.getHistoricalX(i)-offsetX)/kZooming), (int)((event.getHistoricalY(i)-offsetY)/kZooming),
+					  event.getHistoricalPressure(i),
+					  event.getHistoricalSize(i));
+		}
+		
+		drawPoint((int)((event.getX()-offsetX)/kZooming), (int)((event.getY()-offsetY)/kZooming), event.getPressure(),
+				  event.getSize());
+		connectPoints();
+    	
+  
+/*    	
         
 //    	canvas.drawCircle(0, 0, event.getSize() * 500, paint);
     	canvas.drawCircle(event.getX(), event.getY(), event.getSize() * 1000, paint);
     	
-    	this.invalidate();
+    	//this.invalidate();
     	
     	
     	mTv.setText(
 				""+(event.getSize()* 500)+
 				"  "+event.getX()+
-				"  "+event.getY()
+				"  "+event.getY()+"   "+event.getHistorySize()
 				);
-
+*/
     	return true;
     	/*
         float x = event.getX() - Var.brushWidth;
@@ -83,10 +185,134 @@ public class IView extends ImageView {
         }    */	
     }
 
+	private void connectPoints() {
+		int n = items.size();
+		
+		for (int i = 0; i < n-1 ; i++){
+			Item item0 = items.get(i);
+			Item item1 = items.get(i+1);
+			if (Math.abs(item0.x - item1.x) > item0.radius*2 || Math.abs(item0.y - item1.y) > item0.radius*2){
+				items.add(i, new Item(item0, item1));
+				i++;
+				n++;
+			}
+				
+		}
+	}
+
+    
+	private void drawPoint(float x, float y, float pressure, float size) {
+		int mCurX;
+		int mCurY;
+		float mCurPressure;
+		float mCurSize;
+		int mCurWidth;
+
+		mCurX = (int)x;
+		mCurY = (int)y;
+		
+		if (size == 0 && pressure != 1)
+			size = pressure/2;
+		
+		mCurPressure = pressure;
+		mCurSize = size;
+		
+		mCurWidth = 1 ; //(int)(mCurSize*(brush.getSize()));
+		if (mCurWidth < 1) mCurWidth = 1;
+		
+		int n = mCurWidth*2;
+		
+		mPaint.setColor(Color.BLUE);
+		if (mBitmap != null)
+			items.add(new Item(mCurX, mCurY, mCurWidth, mBrush)); //
+	
+	}
+    
 	public void setT(TextView tv) {
 		mTv = tv;
 		
+
+		
 	}
 	
+	private void set(int i, int j, Pixel pixel, int alpha) {
+		if (pixel == null) return;
+		
+		r[i][j] = (byte)pixel.red;
+		y[i][j] = (byte)pixel.yellow;
+		b[i][j] = (byte)pixel.blue;
+		w[i][j] = (byte)pixel.white;
+		t[i][j] = (byte)alpha;
+		
+		modified[i][j] = true;
+	}
+
+	public void paint()
+	{
+	
+		for (Item item : items){
+			for (int i = -item.radius; i <= item.radius ; i++)
+				for (int j = -item.radius; j <= item.radius ; j++){
+					int x1 = item.x+i;
+					int y1 = item.y+j;
+					if (x1 >= 0 && y1 >=0 && x1 < width && y1 < height && !modified[x1][y1]){
+						
+						int brushColor = 0; //brush.getColor(i+item.radius, j+item.radius, item.radius);
+						
+						if (brushColor != -1 && (i)*(i)+(j)*(j) < item.radius2){
+							set(x1, y1, item.pixel, item.alpha);
+							
+							mPaint.setColor(Color.BLACK); //item.rgb
+							mPaint.setAlpha(item.alpha);
+							mCanvas.drawPoint(x1, y1, mPaint);
+						}
+						
+					}
+				}
+		}
+		
+		items.clear();
+		invalidate();
+	}
+	
+	public void setBG(int idBG)
+	{
+		this.idBG = idBG;
+		mBitmapBg = BitmapFactory.decodeResource( getContext().getResources(), idBG);
+		mRectBG = new Rect(0, 0,  mBitmapBg.getWidth(), mBitmapBg.getHeight());
+	}
+
+	@Override 
+	protected void onDraw(Canvas canvas) {
+		canvas.drawColor(Color.BLACK);
+		
+		if (isInEditMode()) return;
+		
+		canvas.drawBitmap(mBitmapBg, mRectBG, mRect, null);
+		if (mBitmap != null) {
+			canvas.drawBitmap(mBitmap, mRectBitmap, mRect, null);
+		}
+		
+		mPaint.setColor(Color.BLUE);
+		mPaint.setTextSize(35);
+		canvas.drawText(""+mBitmap.getHeight()+"  "+mBitmap.getWidth(), 10, 40, mPaint);
+		
+	}
+	
+	public void refresh()
+	{
+		/*
+		mCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
+		for (int x = 0; x < width; x++)
+			for (int z = 0; z < height; z++)
+				mBitmap.setPixel(x, z, Var.ryb2rgb(r[x][z], 
+													 y[x][z],
+													 b[x][z],
+													 w[x][z]));
+		*/
+
+		invalidate();
+	}
+
 
 }
--- TStest/src/com/alexmochalov/tstest/MainActivity.java
+++ TStest/src/com/alexmochalov/tstest/MainActivity.java
@@ -1,7 +1,14 @@
 package com.alexmochalov.tstest;
 
+import java.io.File;
+
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -11,17 +18,20 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
     
+	/** How often to repaint the contents of the window (in ms). */
+    static private final int REPAINT_DELAY = 10;
+	/** Used as a pulse to gradually fade the contents of the window. */
+    static private final int REPAINT_MSG = 1;
+
+    private IView viewCanvas;
+
+	private Brush mBrush;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		View view = findViewById(R.id.entire_view); 
-
-		final TextView tv = findViewById(R.id.hello_world);
-		
-		final IView imageView1 = (IView)findViewById(R.id.imageView1);
-		imageView1.setT(tv);
 		
 	}
 
@@ -32,6 +42,58 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+//		// The ViewCanvas doesnt have its own method onResume
+//		viewCanvas.onResume(PreferenceManager.getDefaultSharedPreferences(this));
+		
+		mBrush = new Brush(this);
+		//Var.brushRadius = prefs.getInt(PREFS_BRUSH_RADIUS, 30);
+		mBrush.setRadius(30, true);
+		
+		viewCanvas = (IView)findViewById(R.id.imageView1);
+		viewCanvas.setBrush(mBrush);
+		
+	    SharedPreferences prefs;
+		prefs =  PreferenceManager.getDefaultSharedPreferences(this);
+		int idBG = prefs.getInt("PREFS_BG", R.drawable.canvas0);
+		
+		viewCanvas.setBG(idBG);
+		
+		
+		startRepainting();
+	}
+
+    void startRepainting() {
+        mHandler.removeMessages(REPAINT_MSG);
+        mHandler.sendMessageDelayed(
+			mHandler.obtainMessage(REPAINT_MSG), REPAINT_DELAY);
+    }
+
+    /**
+     * Stop the pulse to repaint the screen.
+     */
+    void stopRepainting() {
+        mHandler.removeMessages(REPAINT_MSG);
+    }
+	
+	Handler mHandler = new Handler() {
+        @Override public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case REPAINT_MSG: {
+						
+                		viewCanvas.paint();						
+						mHandler.sendMessageDelayed(
+                            mHandler.obtainMessage(REPAINT_MSG), REPAINT_DELAY);
+						break;
+					}
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    };
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
